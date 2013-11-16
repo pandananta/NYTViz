70e061008e9e98b9e58de1861197cb738cfa4de4
Ananta Pandey <pandananta@gmail.com>
2013-11-16 18:20:36 -0500

    added fun graph to intro

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,6 @@
-var pace = 1500;
-var lead = "<h1>Others argue we were always dumb. The media is just accomodating.</h1";
-var takeQuiz = "<h1>So how Dumb does NYT Think You Are? Take This Quiz to Find Out!</h1><br><button class=\"btn btn-large btn-success\" id=\"quiz\" onClick=\"startQuiz()\"> CLICK ME </button>";
+var pace = 7000;
+var lead = "<h1>Others argue we were always dumb. The media is just accomodating.</h1><br><img id=\"imggrph\" src=\"assets/images/graph.png\">";
+var takeQuiz = "<h1>So how Dumb does the New York Times Think You Are? Take This Quiz to Find Out!</h1><br><button class=\"btn btn-large btn-success\" id=\"quiz\" onClick=\"startQuiz()\"> CLICK ME </button>";
 
 $(document).ready(function()
 {   
